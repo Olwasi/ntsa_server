36addb9a4a2ed397cc6c71b002172712439fd8f3
OLWASI LINDA <159750524+Olwasi@users.noreply.github.com>
2026-06-09 14:35:18 +0300

    Update server.py

--- server.py
+++ server.py
@@ -130,10 +130,7 @@ NTSA Traffic Monitoring Division
             img_part  = MIMEImage(img_bytes, name=f"evidence_{fine_ref}.jpg")
             msg.attach(img_part)
 
-        with smtplib.SMTP("smtp.gmail.com", 587) as srv:
-            srv.ehlo()
-            srv.starttls()
-            srv.ehlo()
+        with smtplib.SMTP_SSL("smtp.gmail.com", 465) as srv:
             srv.login(GMAIL_ADDRESS, GMAIL_APP_PASSWORD)
             srv.sendmail(GMAIL_ADDRESS, driver_email, msg.as_string())
 
